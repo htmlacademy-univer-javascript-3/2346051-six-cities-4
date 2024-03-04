5cbba50f2fc83d73504974d83a8da976159c3cf7
mayonnaise <90057279+Mayanzev@users.noreply.github.com>
2024-03-04 20:16:12 +0300

    module-3-task1

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-import Settings from './const';
+import { Settings } from './const';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
---
+++ src/pages/not-found-screen/not-found-screen.tsx
@@ -0,0 +1,9 @@
+function NotFoundScreen(): JSX.Element {
+  return (
+    <section className="error__screen">
+      <h1>404 Not Found</h1>
+      <a href="/">Вернуться на главную</a>
+    </section>
+  );
+}
+export default NotFoundScreen;
