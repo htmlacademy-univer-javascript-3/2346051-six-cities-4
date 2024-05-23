13fd3f37923afd5544742cf428ba6f4025db6dfc
mayonnaise <90057279+Mayanzev@users.noreply.github.com>
2024-05-23 18:13:22 +0300

    favorites empty page

--- src/pages/favorites-screen/favorites-screen.tsx
+++ src/pages/favorites-screen/favorites-screen.tsx
@@ -15,6 +15,7 @@ function FavoritesScreen(): JSX.Element {
   const favoriteOffers = useAppSelector(getOffers).filter((offer) => favoritesOffersId.includes(offer.id));
   const favoriteOffersCitiesSet = new Set(favoriteOffers.map((of) => of.city.name));
   const favoriteOffersCities = Array.from(favoriteOffersCitiesSet);
+  const favorites = useAppSelector(getFavoriteOffersId);
   const dispatch = useAppDispatch();
 
   const handleCityClick = (city: string) => {
@@ -25,27 +26,41 @@ function FavoritesScreen(): JSX.Element {
   return (
     <div className="page">
       <Header />
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              {favoriteOffersCities.map((city) => (
-                <li className="favorites__locations-items" key={city}>
-                  <div className="favorites__locations locations locations--current">
-                    <div className="locations__item">
-                      <a className="locations__item-link" onClick={() => handleCityClick(city)}>
-                        <span>{city}</span>
-                      </a>
+      {favorites.length !== 0 ? (
+        <main className="page__main page__main--favorites">
+          <div className="page__favorites-container container">
+            <section className="favorites">
+              <h1 className="favorites__title">Saved listing</h1>
+              <ul className="favorites__list">
+                {favoriteOffersCities.map((city) => (
+                  <li className="favorites__locations-items" key={city}>
+                    <div className="favorites__locations locations locations--current">
+                      <div className="locations__item">
+                        <a className="locations__item-link" onClick={() => handleCityClick(city)}>
+                          <span>{city}</span>
+                        </a>
+                      </div>
                     </div>
-                  </div>
-                  <OfferList offers={favoriteOffers.filter((o) => o.city.name === city)} listType={typeOfCardList.favourites} />
-                </li>
-              ))}
-            </ul>
+                    <OfferList offers={favoriteOffers.filter((o) => o.city.name === city)} listType={typeOfCardList.favourites} />
+                  </li>
+                ))}
+              </ul>
+            </section>
+          </div>
+        </main>
+      ) : (
+      <main className="page__main page__main--favorites page__main--favorites-empty">
+        <div className="page__favorites-container container">
+          <section className="favorites favorites--empty">
+            <h1 className="visually-hidden">Favorites (empty)</h1>
+            <div className="favorites__status-wrapper">
+              <b className="favorites__status">Nothing yet saved.</b>
+              <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
+            </div>
           </section>
         </div>
       </main>
+      )}
       <footer className="footer container">
         <Link to={AppRoute.Main} className="header__logo-link">
           <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
